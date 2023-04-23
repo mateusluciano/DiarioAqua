bffc3f88801b5ff9aa9804430ee405994f335305
Mateus de Oliveira Luciano <71831859+mateusluciano@users.noreply.github.com>
2023-04-23 14:46:57 -0300

    Calculadora finalizada!

--- js/calculadora.js
+++ js/calculadora.js
@@ -60,10 +60,50 @@ function calcularMetaAgua() {
   
 
   // atualiza o texto do elemento h4 com o resultado
-  if (resultado<2) {
-    metaAgua.textContent = `Tome o mínimo ideal de 2 litros de água por dia`;  
-  } else {
-    metaAguaView.textContent = `Sua meta é de ${resultado.toFixed(1)} litros de água por dia`;
+  if (resultado < 2) {
+    metaAguaView.innerHTML = `Sua meta é de 2 litros<br>de água por dia!`;
+
+    var garrafa2l = document.querySelector('#garrafas');
+    garrafa2l.style.backgroundImage = 'url(../imagens/water/Bottles2l.png)';
+
+    var fraseGarrafas = document.querySelector('#squee')
+    fraseGarrafas.innerHTML = `beba 4 garrafas de 500<small>ml</small>`
+} else {
+    metaAguaView.innerHTML = `Sua meta é de ${resultado.toFixed(1)} litros<br>de água por dia!`;
   }
 
+  // atualiza bg com quantidade de garrafas
+  if (resultado > 2) {
+    resultado = resultado * 1000
+
+    let quantidade = resultado / 500; // Cálculo da quantidade de garrafas
+
+    quantidade = Math.ceil(quantidade); // Arrendonda a quantidade para cima
+
+    var fraseGarrafas = document.querySelector('#squee')
+    fraseGarrafas.innerHTML = `beba ${quantidade} garrafas de 500<small>ml</small>`
+
+    // backgrounds
+    if (resultado > 2000 && resultado <= 2500) {
+      var garrafa25 = document.querySelector('#garrafas');
+      garrafa25.style.backgroundImage = 'url(../imagens/water/Bottles2.5l.png)';
+
+    } else if (resultado > 2500 && resultado <= 3000) {
+      var garrafa30 = document.querySelector('#garrafas');
+      garrafa30.style.backgroundImage = 'url(../imagens/water/Bottles3l.png)';
+
+    } else if (resultado > 3000 && resultado <= 3500) {
+      var garrafa35 = document.querySelector('#garrafas');
+      garrafa35.style.backgroundImage = 'url(../imagens/water/Bottles3.5l.png)';
+
+    } else if (resultado > 3500 && resultado <= 4000) {
+      var garrafa40 = document.querySelector('#garrafas');
+      garrafa40.style.backgroundImage = 'url(../imagens/water/Bottles4l.png)';
+
+    } else {
+      var garrafaMuito = document.querySelector('#garrafas');
+      garrafaMuito.style.backgroundImage = 'url(../imagens/water/BottlesMais4l.png)';
+    }
+
+  }
 }
--- js/inputsJuntos.js
+++ js/inputsJuntos.js
@@ -3,20 +3,20 @@ const pesorange = document.getElementById('pesoSlide');
 
 pesoescrito.addEventListener('input', function() {
   if (pesoescrito.value > 99){
-    pesorange.style.width = '150px'
+    pesoescrito.style.width = '150px'
     pesorange.value = this.value;
   } else {
-    pesoescrito.style.width = '85px'
+    pesoescrito.style.width = '95px'
     pesorange.value = this.value;
   }
 });
 
 pesorange.addEventListener('input', function() {
   if (pesorange.value > 99){
-    pesoescrito.style.width = '150px'
+    pesoescrito.style.width = '150px'    
     pesoescrito.value = this.value;
   } else {
-    pesoescrito.style.width = '85px'
+    pesoescrito.style.width = '95px'
     pesoescrito.value = this.value;
   }
 });
