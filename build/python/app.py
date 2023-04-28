352523fc96831549726c4bc9734bda4d51d2f64b
Mateus de Oliveira Luciano <71831859+mateusluciano@users.noreply.github.com>
2023-04-28 11:44:19 -0300

    test python

--- build/python/app.py
+++ build/python/app.py
@@ -1,7 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+import time
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///visitors.db'
+db = SQLAlchemy(app)
+
+class Visitor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    def __repr__(self):
+        return f'<Visitor {self.id}>'
+
+db.create_all()
 
 @app.route('/')
-def home():
-    return 'Olá, Flask!'
+def index():
+    return render_template('index.html')
+
+@app.route('/add_visitor')
+def add_visitor():
+    visitor = Visitor()
+    db.session.add(visitor)
+    db.session.commit()
+    return jsonify({"message": "Visitante adicionado com sucesso!"})
+
+@app.route('/visitor_count')
+def visitor_count():
+    count = Visitor.query.count()
+    return jsonify({"count": count})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
