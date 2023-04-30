3b2432b6c8418f074b4296971abcadb0ed41530b
Mateus de Oliveira Luciano <71831859+mateusluciano@users.noreply.github.com>
2023-04-30 14:04:00 -0300

    teste database

--- build/server.js
+++ build/server.js
@@ -9,9 +9,15 @@ const client = new Client({
   port: 5432, // padrão para PostgreSQL
 });
 
-async function connect() {
-  await client.connect();
-  console.log('Conectado ao banco de dados PostgreSQL');
-}
-
-connect();
+app.get('/', async (req, res) => {
+  try {
+    await client.connect();
+    const result = await client.query('SELECT * FROM tempo_user');
+    res.send(result.rows);
+  } catch (error) {
+    console.error('Erro ao buscar usuários no banco de dados', error);
+    res.status(500).send('Erro ao buscar usuários no banco de dados');
+  } finally {
+    await client.end();
+  }
+});
